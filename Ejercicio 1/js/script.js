b790987e2eda5428d44edba65ccd5f441ba9248e
Andryk Lara <lara.cordero@gmail.com>
2020-07-21 16:01:32 -0600

    Primeer Ejercicio

--- Ejercicio 1/js/script.js
+++ Ejercicio 1/js/script.js
@@ -1,7 +1,7 @@
 let a = parseFloat(prompt('Ingrese un primer numero para A'));
 let b = parseFloat(prompt('Ingrese un segundo numero para B'));
 
-function comparar(a, b){
+function max(a, b){
     if (a > b){
         return ('A es mayor que B');
     }else if (a == b){
@@ -11,5 +11,5 @@ function comparar(a, b){
     }
 }
 
-let resultado = comparar(a, b);
+let resultado = max(a, b);
 console.log(resultado);
